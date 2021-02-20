e957e3f245305e26cfffc4ea482bde3033c290f8
David Wu <dwu97@yahoo.com>
2021-02-20 12:11:18 -0500

    Refactor with ES6

--- designs.js
+++ designs.js
@@ -1,67 +1,59 @@
 // Select color input
-var chosenColor = function() 
-{
-    return $("#colorPicker")[0].value;
-};
+const chosenColor = function() { 
+  return $("#colorPicker")[0].value; 
+}
 
 // Select size input
-var numRows = function() 
-{
-    return Number($("#input-height")[0].value);
-};
-var numCols = function() 
-{
-    return Number($("#input-width")[0].value);
-};
+const numRows = function() { 
+  return Number($("#input-height")[0].value); 
+}
+const numCols = function() { 
+  return Number($("#input-width")[0].value); 
+}
 
 // Makes grid
-function makeGrid() 
-{
-    if(numRows() >= Number($("#input-height")[0].max))
-    {
-        $("#input-height")[0].value = $("#input-height")[0].max;
+const makeGrid = function() {
+  // Limit grid size
+  if (numRows() >= Number($("#input-height")[0].max)) {
+    $("#input-height")[0].value = $("#input-height")[0].max;
+  }
+  if (numCols() >=  Number($("#input-width")[0].max)) {
+    $("#input-width")[0].value = $("#input-width")[0].max;
+  }
+
+  // Fill in grid
+  const pixelCanvas = $("#pixel-canvas");
+
+  // Create pixel canvas table
+  pixelCanvas.empty();
+  for (let i = 0; i < numRows(); i++) {
+    const row = pixelCanvas[0].insertRow(i);
+    for (let j = 0; j < numCols(); j++) {
+      const cell = row.insertCell(j);
     }
-    if(numCols() >=  Number($("#input-width")[0].max))
-    {
-        $("#input-width")[0].value = $("#input-width")[0].max;
+  }
+
+  // Paint
+  pixelCanvas.on("mouseover mousedown", (evt) => {
+    evt.preventDefault();
+    // Paint with chosen color with left click
+    if (evt.which === 1) {
+      $(evt.target).css("background-color", chosenColor());
     }
-    var myTable = $("#pixel-canvas");
-    myTable.empty();
-    for(var i = 0; i < numRows(); i++)
-    {
-        var row = $("<tr></tr>");
-        myTable.append(row);
-        for(var j = 0; j < numCols(); j++)
-        {
-            var col = $("<td></td>");
-            row.append(col);           
-        }
+    // Remove paint with right click
+    if (evt.which === 3) {
+      $(evt.target).css("background-color", "white");
     }
-};
+  });
+
+  // Disable right click menu within grid
+  pixelCanvas.on("contextmenu", () => {
+    return false;
+  });
+}
 
 // When size is submitted by the user, call makeGrid()
-var pushButton = $("#push-button");
-pushButton.on("click",function(evt)
-{
-    makeGrid();
-});
-
-// Paint
-var box = $("table");
-box.on("mouseover mousedown",function(evt)
-{evt.preventDefault();
-    if(evt.which === 1)
-    {
-        var myColor = chosenColor();
-        $(evt.target).css("background-color",chosenColor());
-    }
-    if(evt.which === 3)
-    {
-        $(evt.target).css("background-color","white");
-    }
-});
-
-// Disable right click menu within grid
-box.on("contextmenu",function(){
-    return false;
+const pushButton = $("#push-button");
+pushButton.on("click", (evt) => {
+  makeGrid();
 });
